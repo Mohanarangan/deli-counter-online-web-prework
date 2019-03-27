61a547f9c4e9af9453afa4ae4f0bc1c3eb3b3b23
chandra mohan <mohaniyer_78@hotmail.com>
2019-03-27 20:36:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 def line(names)
   if names == [] 
   puts "The line is currently empty."
-  else
+ else
    string = "The line is currently:"
   names.each_with_index do |name, index|
     string += " "+"#{index+1}." + " "+ name 
@@ -21,10 +21,11 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty? == true
+  while katz_deli.empty? == true
   puts "There is nobody waiting to be served!"
-else
-  puts " Currently serving #{katz_deli.first}."
+
+end
+puts " Currently serving #{katz_deli.first}."
 end
 
 
@@ -43,6 +44,33 @@ end
 
 
 
+=======
+    string = 
+  names.each do |name|
+    
+    
+    
+  end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  end
+  
+  #def line(other_deli)
+    #puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  #end
+  #def line(another_deli)
+   # puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
+    #end
+end
+
+>>>>>>> 655e8aed4c796e5f97c9a0d317551edfd9f0d37d
 
 
 
